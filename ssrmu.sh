31d3838288903cab891e0d8f62076ad8e681d5b4
Crazyxx <12903636+xlf1234@users.noreply.github.com>
2018-11-27 19:44:14 +0800

    Update ssrmu.sh

--- ssrmu.sh
+++ ssrmu.sh
@@ -900,11 +900,11 @@ JQ_install(){
 		if [[ ${bit} = "x86_64" ]]; then
 			#mv "jq-linux64" "jq"
 			#wget --no-check-certificate "https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64" -O ${jq_file}
-			wget --no-check-certificate "https://github.com/xlf1234/doubi/blob/master/jq-linux64" -O ${jq_file}
+			wget --no-check-certificate "https://cloud.vccvps.com/jq-linux64" -O ${jq_file}
 		else
 			#mv "jq-linux32" "jq"
 			#wget --no-check-certificate "https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux32" -O ${jq_file}
-			wget --no-check-certificate "https://github.com/xlf1234/doubi/blob/master/jq-linux32" -O ${jq_file}
+			wget --no-check-certificate "https://cloud.vccvps.com/jq-linux32" -O ${jq_file}
 		fi
 		[[ ! -e ${jq_file} ]] && echo -e "${Error} JQ解析器 重命名失败，请检查 !" && exit 1
 		chmod +x ${jq_file}
